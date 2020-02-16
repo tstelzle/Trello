fc427a029190eb2f8639dbdc0cbaffbf7557e455
tstelzle <tarek.stelzle@gmx.de>
2020-02-16 10:14:24 +0100

    new trello.py

---
+++ trello2.py
@@ -0,0 +1,26 @@
+import json
+import csv
+import sys
+
+with open(sys.argv[1]) as f:
+    data = json.load(f)
+
+myLists = {}
+
+for elem in data["lists"]:
+    myLists[elem["id"]] = [elem["name"]]
+    #print(elem["id"] +  elem["name"])
+
+for elem in data["cards"]:
+    myLists[elem["idList"]].append(elem["name"])
+
+
+
+myFile = open(sys.argv[2], 'w')
+
+with myFile:
+    writer = csv.writer(myFile)
+    writer.writerows(myLists.values())
+
+for (k,v) in myLists.items():
+    print(v)
